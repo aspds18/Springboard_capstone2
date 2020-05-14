b54d1f194f9b8bd6f15bc676a4b5a1c25b782dd6
Silvia <43660781+aspds18@users.noreply.github.com>
2020-05-13 21:18:57 -0700

    tune and plot functions added

--- surv_comp.py
+++ surv_comp.py
@@ -3,82 +3,112 @@ from sksurv.linear_model import CoxPHSurvivalAnalysis
 from sksurv.svm import FastSurvivalSVM
 from sksurv.ensemble import RandomSurvivalForest
 
+from sklearn.model_selection import train_test_split,GridSearchCV
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def prepare(X,y):
+def xy(dataFr):
     
-    '''reshape data to be used by the various models from Scikit-survival package'''
+    '''create features and target for this specific project'''
     
-    # Kaplan Meier requires only an event column and a duration
-    E=y.copy().astype('bool')
-    T= X['duration']
+    y=dataFr['status_group']
+    y=y.apply(lambda val: 1 if val!=0 else 0)
+    X=dataFr.drop(columns=['status_group'])
+    return X,y
+
+
+def split_data(X,y,random=9,size=0.3):
     
-    # Cox and SVM require the target to have 2 columns, the first one being the event, the second one the duration
-    Xc=X.copy()
-    #Xc.drop(columns=['construction_year'],inplace=True,axis=1)
-    #y1=y.copy().reset_index(drop=True)
-    #l=[(e,t) for e,t in zip(y1,Xc['duration'])]
-    y1=y.copy()
-    l=[(e,t) for e,t in zip(y1,Xc['duration'])]
-    yc=np.array(l,'bool,i')
-    Xc.drop(columns=['duration'],axis=1,inplace=True)
+    '''split data into training and test set'''
     
-    # Random Survival Forest requires the second column in the target to be in double format
-    # and the features to be in a structure format
-    Xrf=np.column_stack(Xc.copy().astype('float').values).T
-    yrf=np.array(l,'bool,double')
-    
-    return E,T,Xc,yc,Xrf,yrf
+    # stratify keeps the proportions of labels 
+    Xtr, Xtst, ytr, ytst = train_test_split(X,y,test_size=size,random_state=random, stratify=y)
+    return Xtr,Xtst,ytr,ytst
 
 
 def surv_data(X,y):
     
     '''reshape data to be used by the various models from Scikit-survival package'''
 
-    #y=y.apply(lambda val: 1 if val!=0 else 0)
-    #X.reset_index(drop=True,inplace=True)
-    #y.reset_index(drop=True,inplace=True)
+    # the time to event or duration is needed in the survival models
     # once the duration is defined, the year features can be dropped, since it would be a redundant information
-    X['duration']=X['recorded_year']-X['construction_year']
+    X=X.assign(duration=-X.loc[:,'construction_year']+X.loc[:,'recorded_year'])
     X.drop(columns=['recorded_year','construction_year'],axis=1,inplace=True)
     itd=X[X['duration']<0].index
     X.drop(itd,axis=0,inplace=True)
     y.drop(itd,axis=0,inplace=True)
     
     # Kaplan Meier requires only an event column and a duration
-    E=y.copy().astype('bool')
+    E=y.astype('bool')
     T= X['duration']
     
     # Cox and SVM require the target to have 2 columns, the first one being the event, the second one the duration
-    Xc=X.copy()
-    y1=y.copy()
-    l=[(e,t) for e,t in zip(y1,Xc['duration'])]
+    Xc=X
+    l=[(e,t) for e,t in zip(y,X['duration'])]
     yc=np.array(l,'bool,i')
     Xc.drop(columns=['duration'],axis=1,inplace=True)
     
     # Random Survival Forest requires the second column in the target to be in double format
     # and the features to be in a structure format
-    Xrf=np.column_stack(Xc.copy().astype('float').values).T
+    Xrf=np.column_stack(Xc.astype('float').values).T
     yrf=np.array(l,'bool,double')
     
     return E,T,Xc,yc,Xrf,yrf
 
+
+def tune_model(base,Xtr,ytr,Xtst,ytst,grid):
     
+    '''grid search of a base model'''
+    
+    surv_cv= GridSearchCV(base, grid, cv=5,refit=True)
+    surv_cv.fit(Xtr,ytr)
+    print("Tuned Parameters: {}".format(surv_cv.best_params_)) 
+    print('')
+    
+    best=surv_cv.best_estimator_
+    print('Training score:',round(best.score(Xtr,ytr),3))
+    print('Test score:',round(best.score(Xtst,ytst),3))
+   
+    return best
+
+
+
+# Kaplan Meier section#    
 def km(event,duration):
         
-    '''Kaplan Meir estimator'''
+    '''Kaplan Meier estimator'''
     
     years, survival_prob = kaplan_meier_estimator(event,duration)
     plt.figure(figsize=(10,6))
     plt.step(years, survival_prob,where='post')
     plt.title('Estimated probability of survival',fontsize=16)
-    plt.ylabel("$\hat{S}(t)$")
-    plt.xlabel("years")
+    plt.ylabel("$\hat{S}(t)$",fontsize=14)
+    plt.xlabel("years",fontsize=14)
     
+def prob_vs_feature(df,feature,E,T):
     
+    '''plot survival probabilty with respect to a specific feature'''
+    
+    plt.figure(figsize=(10,6))
+    plt.title('Estimated probability of survival by {}'.format(feature),fontsize=16)
+    for ft in df[feature].unique().tolist():
+        mask = df[feature] == ft
+        time, survival_prob = kaplan_meier_estimator(
+            E[mask],
+            T[mask])
+    
+        plt.step(time, survival_prob, where="post",
+             label=ft)
+    
+    plt.ylabel("$\hat{S}(t)$",fontsize=14)
+    plt.xlabel("years",fontsize=14)
+    plt.legend(loc="best")
+
+    
+# Cox Regression section#    
 def cox(X,y):
     
     '''Cox regression model'''
@@ -90,8 +120,38 @@ def cox(X,y):
     print('Features coefficients:')
     coeff=pd.Series(cox.coef_, index=X.columns)
     print(coeff)
+    return cox
+
+def cox_prediction(index,model,X):
     
+    '''make prediction using a trained Cox regression model'''
     
+    Xp=X.iloc[index]
+    surv=model.predict_survival_function(Xp)
+    haz=model.predict_cumulative_hazard_function(Xp)
+    return surv,haz
+    
+def plot_cox_prediction(model,sur,haz):
+    
+    '''plot the survival probability and hazard function of Cox model'''
+    
+    fig,axs=plt.subplots(1,2,figsize=(12,5))
+    plt.subplots_adjust(wspace=0.35)
+    fig.subplots_adjust(top=0.9)
+    
+    plt.subplot(1, 2, 1)
+    plt.title("Survival probability ",fontsize=16)
+    plt.xlabel("Years",fontsize=14)
+    for i, c in enumerate(sur):
+        plt.step(c.x, c.y, where="post")
+                
+    plt.subplot(1,2,2)    
+    plt.title("Cumulative hazard",fontsize=16)
+    plt.xlabel("Years",fontsize=14)
+    for i, c in enumerate(haz):
+        plt.step(c.x, c.y, where="post")
+    
+# Support vector machine section    
 def ssvm(X,y):
     
     '''Support Vectors applied to survival analyses'''
@@ -100,7 +160,9 @@ def ssvm(X,y):
     svmf=svm.fit(X, y)
     print('Svm score:', svmf.score(X, y))
     
-        
+    return svmf
+    
+# Random Survival Forest section        
 def srf(X,y,random):
     
     '''Random Forest applied to survival analyses'''    
@@ -113,6 +175,61 @@ def srf(X,y,random):
                            random_state=random)
     rsf.fit(X, y)
     print('Random Forest score:', rsf.score(X, y))
+    
+    return rsf
+
+def rf_prediction(index,model,X):
+    
+    '''make prediction using a trained Random Survival Forest'''
+    
+    #X is the array of features, index is the observation
+    Xp=X[index].reshape(-1,1).T
+    surv=model.predict_survival_function(Xp)
+    haz=model.predict_cumulative_hazard_function(Xp)
+    
+    return surv,haz
 
     
+def plot_rf_prediction(model,sur,haz):
     
+    '''plot the survival probability and hazard function of Random Forest'''
+    
+    fig,axs=plt.subplots(1,2,figsize=(12,5))
+    plt.subplots_adjust(wspace=0.35)
+    fig.subplots_adjust(top=0.9)
+    plt.subplot(1, 2, 1)
+    plt.step(model.event_times_, sur.T, where="post")
+    plt.title("Survival probability",fontsize=16)
+    plt.xlabel("Years",fontsize=14)
+    plt.subplot(1, 2, 2)
+    plt.step(model.event_times_, haz.T, where="post")
+    plt.title("Cumulative hazard",fontsize=16)
+    plt.xlabel("Years",fontsize=14)   
+    
+    
+    
+def plot_predictions(rf,rf_sur,rf_haz,cox_sur,cox_haz):
+    
+    '''compare survival probability and hazard function of Random Survival Forest and Cox'''
+    
+    fig,axs=plt.subplots(1,2,figsize=(12,5))
+    plt.subplots_adjust(wspace=0.35)
+    fig.subplots_adjust(top=0.9)
+    plt.subplot(1, 2, 1)
+    plt.title("Survival probability ",fontsize=16)
+    plt.xlabel("Years",fontsize=14)
+    
+    plt.step(rf.event_times_, rf_sur.T, where="post",label='Random Forest')
+    for i, c in enumerate(cox_sur):
+        plt.step(c.x, c.y, where="post",label='Cox')
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.title("Cumulative hazard",fontsize=16)
+    plt.xlabel("Years",fontsize=14)
+    
+    plt.step(rf.event_times_, rf_haz.T, where="post",label='Random Forest')
+    for i, c in enumerate(cox_haz):
+        plt.step(c.x, c.y, where="post",label='Cox')
+    plt.legend()
+        
